ea0ad829cf7b571910a47cdd179c7de2a6ec1152
Vladyslav Natalych <v.natalich05@gmail.com>
2024-10-10 23:42:52 +0300

    initial

---
+++ src/apiService/fetchCardData.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// const myApiKey = "33J-hHOVDtSzhZefII_e_EYyB10rzckrD6pig0C11YI";
+
+// axios.defaults.baseURL = 'https://api.unsplash.com/';
+
+// const fetchData = async () => {
+//     const response = await axios.get(baseURL);
+// };
